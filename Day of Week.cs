7c55832d0daeb877452cbf83895f0ca967059766
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-29 16:18:32 +0300

    Add files via upload

---
+++ Day of Week.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number =  int.Parse(Console.ReadLine());
+            string[] days = {"Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday", "Sunday" };
+            if (number < 1 || number > 7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[number - 1]);
+            }
+        }
+    }
+}
