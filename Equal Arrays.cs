cbf4dd849337b9fd7831d7c5bca158c20a042aae
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-30 23:28:14 +0300

    Add files via upload

---
+++ Equal Arrays.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace _7._Equal_Arrays
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            int[] arr2 = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int counter = 0;
+            int sum = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                sum += arr1[i];
+                if (arr1[i] == arr2[i])
+                {
+                    counter++;
+                    if (counter == arr1.Length)
+                    {
+                        Console.WriteLine($"Arrays are identical. Sum: {sum}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index.");
+                    break;
+                }
+            }
+        }
+    }
+}
