e4da73f4b817cc7ec5f5597a9db1799bc134334f
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-30 21:30:39 +0300

    Add files via upload

---
+++ Reverse Array of Strings.cs
@@ -0,0 +1,29 @@
+﻿
+namespace _4._Reverse_Array_of_Strings
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       //Първи начин
+            string[] arr = Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+            
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                //string currentLetter = arr[i];
+                Console.Write(arr[i]);
+                Console.Write($" ");
+            }
+
+
+
+                       //Втори начин
+            
+           
+        }
+    }
+}
