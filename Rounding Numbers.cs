4ff87a4130a2e25d2c4fe82f80a3ca00fc8c1235
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-30 21:20:33 +0300

    Add files via upload

---
+++ Rounding Numbers.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _3._Rounding_Numbers
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToArray();
+            int[] roundNumbers = new int[arr.Length];
+            for (int i = 0; i < roundNumbers.Length; i++)
+            {
+               roundNumbers[i] = (int)Math.Round(arr[i], MidpointRounding.AwayFromZero);
+               Console.WriteLine($"{arr[i]} => {roundNumbers[i]}");
+            }
+            
+
+
+
+        }
+    }
+}
