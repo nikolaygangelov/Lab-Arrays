a1c99e0416858a8f7e0489b45a4af1f25dc37e20
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-10-03 20:28:35 +0300

    Add files via upload

---
+++ Condense Array to Number.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace _8._Condense_Array_to_Number
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine()//3
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            //2
+            while (arr1.Length > 1)
+            {
+                int[] arr2 = new int[arr1.Length - 1];
+                for (int i = 0; i < arr1.Length - 1; i++)
+                {
+                    arr2[i] = arr1[i] + arr1[i + 1];
+                }
+                arr1 = arr2;
+            }
+            Console.WriteLine(arr1[0]);
+        }
+    }
+}
